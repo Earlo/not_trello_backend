663b870c4fa54e0d73934430c4f71dd89a26bd96
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2019-12-14 21:40:18 +0200

    Create list endpoint

--- routes/columns.js
+++ routes/columns.js
@@ -8,19 +8,28 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/add').post((req, res) => {
-  const title = req.body.title;
-  const color = req.body.color;
-  const priority = Number(req.body.priority);
-
-  const newColumn = new Column({
-    title,
-    color,
-    priority,
-  });
-
-  newColumn.save()
-    .then(() => res.json('Column added!'))
-    .catch((err) => res.status(400).json(`Error: ${err}`));
+  const { title } = req.body;
+  const { color } = req.body;
+  let priority = 0;
+  Column
+    .findOne({})
+    .sort('-priority')
+    .exec((err, member) => {
+      if (err) {
+        priority = 0;
+      }
+      if (member) {
+        priority = member.priority + 1;
+      }
+      const newColumn = new Column({
+        title,
+        color,
+        priority,
+      });
+      newColumn.save()
+        .then(() => res.json('Column added!'))
+        .catch((serr) => res.status(400).json(`Error: ${serr}`));
+    });
 });
 
 router.route('/:id').get((req, res) => {
